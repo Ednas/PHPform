afb94f808ee408b74785048749cd7a6ec9861048
Ednas <edna@edna.tech>
2017-05-17 22:32:25 -0400

    Added index, header and footer file

---
+++ index.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html
+<head>
+  <?php require './head.php' ?>
+</head>
+<body>
+  <div class="container">
+  <?php 
+
+    require './nav.php';
+    require './content.php';
+    require './footer.php';
+
+  ?>
+  </div>
+</body>
+</html>
